53efe10b75ff5e695cfa38f331030ba88d77bd22
Janice <carmeljanice825@gmail.com>
2024-08-03 10:46:41 +0530

    loading page and error page added (TASK-2)

---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="p-40">
+            <p className="font-black text-5xl p-4 pl-0">404 - Page Not Found</p>
+            <p className="font-bold text-xl text-yellow-200">The page you are looking for does not exist.</p>
+        </div>
+    )
+}
